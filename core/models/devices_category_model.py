31d9bb030fe93d07403fee87767fe5ace433fdc0
Fiskoff <fiskoff2015@yandex.ru>
2025-10-27 16:54:09 +0700

    created model devices

--- core/models/devices_category_model.py
+++ core/models/devices_category_model.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import VARCHAR
 
 from core.models.base_model import BaseModel
 
@@ -8,4 +8,6 @@ class DevicesCategoryModel(BaseModel):
     __tablename__ = "Devices_category"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    category_name: Mapped[str] = mapped_column(String(256))
+    category_name: Mapped[str] = mapped_column(VARCHAR(256))
+
+    devices: Mapped[list["Devices"]] = relationship(back_populates="category")
