bc087a9df1df93964f48db58e8d4b862b7327480
Fiskoff <fiskoff2015@yandex.ru>
2025-10-27 13:03:17 +0700

    Created root endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from uvicorn import run
 
 from core.config import settings
@@ -11,7 +11,14 @@ logger = logging.getLogger(__name__)
 
 def create_application() -> FastAPI:
     app = FastAPI(title="IlumaStore", version="1.0.0", docs_url="/docs", redoc_url="/redoc")
-    app.include_router(...)
+    router = APIRouter(tags=["root"])
+    @router.get("/")
+    async def root():
+        return {
+            "message": "IlumaStore API",
+            "swagger": "/docs"
+        }
+    app.include_router(router)
     return app
 
 main_app = create_application()
