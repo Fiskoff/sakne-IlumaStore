f99fb2561fb89d1fc54e6bfe67a405eab15da781
Fiskoff <fiskoff2015@yandex.ru>
2025-10-27 18:25:52 +0700

    run migration

--- core/migrations/env.py
+++ core/migrations/env.py
@@ -9,6 +9,12 @@ from alembic import context
 
 from core.config import settings
 from core.models.base_model import BaseModel
+from core.models.iqos_model import IqosModel
+from core.models.iqos_category_model import IqosCategoryModel
+from core.models.terea_model import TereaModel
+from core.models.terea_category_model import TereaCategoryModel
+from core.models.devices_model import DevicesModel
+from core.models.devices_category_model import DevicesCategoryModel
 
 
 config = context.config
