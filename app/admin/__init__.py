45e16ad4c73b668941f36f03748169d13ad13189
Fiskoff <fiskoff2015@yandex.ru>
2025-10-30 14:28:25 +0700

    Updated iqos model and category model for Admin

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,11 +1,8 @@
-from app.admin.admin_models import (
-    IqosAdmin,
-    TereaAdmin,
-    DevicesCategoryAdmin,
-    IqosCategoryAdmin,
-    TereaCategoryAdmin
-)
 from app.admin.devices_admin_model import DevicesAdmin
+from app.admin.iqos_admin_model import IqosAdmin
+from app.admin.terea_admin_model import TereaAdmin
+from app.admin.category_admin_madel import DevicesCategoryAdmin, IqosCategoryAdmin, TereaCategoryAdmin
+
 
 admin_views = [
     DevicesAdmin,
